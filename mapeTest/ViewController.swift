f34ec532cc220367b13322b78e0af7b56a324fd8
akash <akash9888soni>
2018-03-18 07:50:49 +0530

    Initial Commit

---
+++ mapeTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mapeTest
+//
+//  Created by ANURAG ADARSH on 18/03/18.
+//  Copyright © 2018 ANURAG ADARSH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
